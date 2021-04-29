e9511fab67284f048f9daf3d5a790dcf238c0e0f
kql2 <ltqsoft@gmail.com>
2021-04-29 22:05:42 +0700

    2021-04-29

---
+++ ep27/AppImages.cc
@@ -0,0 +1,81 @@
+#include "Application.hh"
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+
+static const char* const TEXTURE_FILE_PATH = "star_butterfly.jpg";
+
+
+void Application::createTexture()
+{
+    int width = 512, height = 512, channels = 4;
+    VkFormat format = VK_FORMAT_R8G8B8A8_UNORM;
+
+    stbi_uc* pixels = stbi_load(TEXTURE_FILE_PATH, &width, &height, &channels, STBI_rgb_alpha);
+    if(pixels == nullptr) throw rterr(ERR_TEXTURE_FILE_LOAD, "Loi: khong the doc file hinh anh!");
+    Buffer temp = this->createTempBuffer(width * height * 4, pixels);
+    stbi_image_free(pixels);
+
+    VkImageCreateInfo imgInfo = {};
+    imgInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+    imgInfo.imageType = VK_IMAGE_TYPE_2D;
+    imgInfo.arrayLayers = 1;
+    imgInfo.extent.width = width;
+    imgInfo.extent.height = height;
+    imgInfo.extent.depth = 1;
+    imgInfo.format = format;
+    imgInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    imgInfo.mipLevels = 1;
+    imgInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+    imgInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    imgInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+    imgInfo.usage = 
+        VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
+        VK_IMAGE_USAGE_TRANSFER_DST_BIT |
+        VK_IMAGE_USAGE_SAMPLED_BIT;
+
+    VkResult result = VK_ERROR_UNKNOWN;
+    vkfunc(result, "Loi: tao image khong thanh cong!",
+        vkCreateImage(device, &imgInfo, nullptr, &texture.id)
+    );
+
+    VkMemoryRequirements memReqs;
+    vkGetImageMemoryRequirements(device, texture.id, &memReqs);
+
+    VkMemoryAllocateInfo allocInfo = {};
+    allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    allocInfo.allocationSize = memReqs.size;
+    allocInfo.memoryTypeIndex = this->getMemType(
+        memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT
+    );
+    vkfunc(result, "Loi: cap phat bo nho cho image khong thanh cong!",
+        vkAllocateMemory(device, &allocInfo, nullptr, &texture.mem)
+    );
+
+    vkfunc(result, "Loi: khong the lien ket image voi memory!",
+        vkBindImageMemory(device, texture.id, texture.mem, 0);
+    )
+
+    texture.view = this->createImageView(texture.id, format, VK_IMAGE_ASPECT_COLOR_BIT);
+
+    VkSamplerCreateInfo samplerInfo = {};
+    samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+    samplerInfo.magFilter = samplerInfo.minFilter = VK_FILTER_LINEAR;
+    samplerInfo.addressModeU = samplerInfo.addressModeV = samplerInfo.addressModeW = 
+        VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+    samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_WHITE;
+    
+    vkfunc(result, "Loi: tao sampler khong thanh cong!",
+        vkCreateSampler(device, &samplerInfo, nullptr, &texture.sampler)
+    );
+
+    this->beginCopy();
+    // bla bla...
+    this->endCopy();
+
+    vkDestroyBuffer(device, temp.id, nullptr);
+    vkFreeMemory(device, temp.mem, nullptr);
+
+    std::cout << "Tao texture thanh cong, nhung chua co du lieu.\n";
+}
---
+++ ep27/data.hh
@@ -0,0 +1,48 @@
+#ifndef DATA_HH
+#define DATA_HH
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/gtc/matrix_transform.hpp>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <cstring>
+
+
+struct Buffer {
+    VkBuffer id;
+    VkDeviceMemory mem;
+    void* mapData;
+};
+
+
+struct UniformBufferData {
+    glm::mat4 viewMatrix;
+    glm::mat4 worldMatrix;
+};
+
+
+struct PushConstData {
+    glm::vec3 lightColor;
+};
+
+
+struct Image {
+    VkImage id;
+    VkDeviceMemory mem;
+    VkImageView view;
+    VkSampler sampler = VK_NULL_HANDLE;
+};
+
+
+struct Viewer {
+    glm::vec3 pos;
+    glm::vec3 front;
+    glm::vec3 up;
+    glm::vec3 right;
+    float moveSpeed;
+    float turnSpeed;
+};
+
+
+#endif
